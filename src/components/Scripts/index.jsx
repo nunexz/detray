d34e734190e9986bf50848d73361ca3d29f1b90e
Matheus Morato <nunes.morato@gmail.com>
2020-09-16 22:52:03 -0300

    stylised was applyed to pages

--- src/components/Scripts/index.jsx
+++ src/components/Scripts/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './styles.css'
 
 const Scripts = () => {
     return (
@@ -6,21 +7,110 @@ const Scripts = () => {
                 <div className='row'>
                     <div className="script-tag">
                         <div className="tag">
-                            <h3>Atendimento</h3>
+                            <h4>Atendimento</h4>
                         </div>
+
                         <div className="script">
                             shuashuashaus
                         </div>
+                    </div>
 
-                        <div className="actions">
-                            <div className="copy">
-                                <button>Copiar</button>
-                            </div>
-                            <div className="edit-delete">
-                                <button>Editar</button>
-                                <button>Excluir</button>
-                            </div>
+                    <div className="actions">
+                        <div className="copy">
+                            <button>Copiar</button>
                         </div>
+                        <div className="edit-delete">
+                            <button>Editar</button>
+                            <button>Excluir</button>
+                        </div>                           
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className="script-tag">
+                        <div className="tag">
+                            <h4>Atendimento</h4>
+                        </div>
+
+                        <div className="script">
+                            shuashuashaus
+                        </div>
+                    </div>
+
+                    <div className="actions">
+                        <div className="copy">
+                            <button>Copiar</button>
+                        </div>
+                        <div className="edit-delete">
+                            <button>Editar</button>
+                            <button>Excluir</button>
+                        </div>                           
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className="script-tag">
+                        <div className="tag">
+                            <h4>Atendimento</h4>
+                        </div>
+
+                        <div className="script">
+                            shuashuashaus
+                        </div>
+                    </div>
+
+                    <div className="actions">
+                        <div className="copy">
+                            <button>Copiar</button>
+                        </div>
+                        <div className="edit-delete">
+                            <button>Editar</button>
+                            <button>Excluir</button>
+                        </div>                           
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className="script-tag">
+                        <div className="tag">
+                            <h4>Atendimento</h4>
+                        </div>
+
+                        <div className="script">
+                            shuashuashaus
+                        </div>
+                    </div>
+
+                    <div className="actions">
+                        <div className="copy">
+                            <button>Copiar</button>
+                        </div>
+                        <div className="edit-delete">
+                            <button>Editar</button>
+                            <button>Excluir</button>
+                        </div>                           
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className="script-tag">
+                        <div className="tag">
+                            <h4>Atendimento</h4>
+                        </div>
+
+                        <div className="script">
+                            shuashuashaus
+                        </div>
+                    </div>
+
+                    <div className="actions">
+                        <div className="copy">
+                            <button>Copiar</button>
+                        </div>
+                        <div className="edit-delete">
+                            <button>Editar</button>
+                            <button>Excluir</button>
+                        </div>                           
                     </div>
                 </div>
             </div>
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Search from '../../components/Search';
 import Scripts from '../../components/Scripts';
-
+import './styles.css'
 
 const Main = () => {
     return (
